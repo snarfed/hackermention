39f704d0630206eb7f18a106b038498741d3c3fe
Ryan Barrett <git@ryanb.org>
2022-08-20 21:34:10 -0700

    discover.py: add threading

--- discover.py
+++ discover.py
@@ -7,24 +7,67 @@ import argparse
 import csv
 import logging
 from pathlib import Path
+import queue
 import sys
+import threading
 
 from oauth_dropins.webutil import util
 from oauth_dropins.webutil.webmention import discover
 from requests.exceptions import RequestException
 
+NUM_THREADS = 50
+
 logging.basicConfig()
 logging.getLogger().setLevel(logging.DEBUG)
 
+endpoints = {}  # maps domain to endpoint
+endpoints_lock = threading.RLock()
+targets = queue.Queue()     # target URLs
+discovered = queue.Queue()  # (domain, endpoint) tuples
+
+parser = argparse.ArgumentParser(description='Discover webmention endpoints.')
+parser.add_argument('--file', '-f', help='input CSV with source,target URLs')
+parser.add_argument('--output', '-o', help='output CSV with domain,endpoint')
+args = parser.parse_args()
+
+
+def discoverer():
+    while True:
+        target = targets.get()
+        domain = util.domain_from_link(target)
+        if not domain:
+            targets.task_done()
+            continue
+
+        if domain not in endpoints:
+            try:
+                endpoint, _ = discover(target)
+            except BaseException as e:
+                if (isinstance(e, (ValueError, RequestException)) or
+                    util.is_connection_failure(e)):
+                    endpoint = None
+                else:
+                    print('!!! Thread dying !!!')
+                    raise
+                logging.warning(target, exc_info=True)
+
+            with endpoints_lock:
+                endpoints[domain] = endpoint
+
+            discovered.put((domain, endpoint))
+
+        targets.task_done()
+
+
+def writer():
+    with open(args.output, 'a', newline='', buffering=1) as f:
+        writer = csv.writer(f, dialect=csv.unix_dialect)
+        while True:
+            writer.writerow(discovered.get())
+            discovered.task_done()
+
 
 def main():
-    parser = argparse.ArgumentParser(description='Discover webmention endpoints.')
-    parser.add_argument('--file', '-f', help='input CSV with source,target URLs')
-    parser.add_argument('--output', '-o', help='output CSV with domain,endpoint')
-    args = parser.parse_args()
-
-    endpoints = {}  # maps domain to endpoint
-
     # read existing endpoints, if any
     outpath = Path(args.output)
     if outpath.exists():
@@ -35,25 +78,18 @@ def main():
         with open(outpath, 'a') as outf:
             outf.write('domain,endpoint\n')
 
-    # discover new endpoints
-    with open(args.file, newline='') as inf, \
-         open(outpath, 'a', newline='', buffering=1) as outf:
-        writer = csv.writer(outf, dialect=csv.unix_dialect)
-        r = csv.reader(inf)
-        for source, target in csv.reader(inf):
-            domain = util.domain_from_link(target)
-            if domain not in endpoints:
-                try:
-                    endpoint, _ = discover(target)
-                except BaseException as e:
-                    if (isinstance(e, (ValueError, RequestException)) or
-                        util.is_connection_failure(e)):
-                        endpoint = None
-                    else:
-                        raise
-                    logging.warning(f'{target}', exc_info=True)
-                endpoints[domain] = endpoint
-                writer.writerow((domain, endpoint))
+    # start worker threads
+    threading.Thread(target=writer, daemon=True).start()
+    for i in range(NUM_THREADS):
+        threading.Thread(target=discoverer, daemon=True).start()
+
+    # enqueue new endpoints
+    with open(args.file, newline='') as f:
+        for _, target in csv.reader(f):
+            targets.put(target)
+
+    targets.join()
+    discovered.join()
 
 
 if __name__ == '__main__':
